8427f8723bb04158fed7e24fa603d2679a5de780
Kenzothd <Kenzothd@hotmail.com>
2022-08-26 17:00:09 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initial Commit</h1>
+    </div>
+  );
+}
+
+export default App;
